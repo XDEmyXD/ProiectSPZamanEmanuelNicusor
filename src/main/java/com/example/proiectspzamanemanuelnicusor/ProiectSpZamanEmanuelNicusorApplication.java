907c47d3a3ca23444bb10619da0b8cf0cb6d019a
XDEmyXD <emanuel.zaman03@e-uvt.ro>
2025-01-20 19:29:52 +0200

    Initial commit

---
+++ src/main/java/com/example/proiectspzamanemanuelnicusor/ProiectSpZamanEmanuelNicusorApplication.java
@@ -0,0 +1,13 @@
+package com.example.proiectspzamanemanuelnicusor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectSpZamanEmanuelNicusorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProiectSpZamanEmanuelNicusorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proiectspzamanemanuelnicusor/ProiectSpZamanEmanuelNicusorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proiectspzamanemanuelnicusor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectSpZamanEmanuelNicusorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
